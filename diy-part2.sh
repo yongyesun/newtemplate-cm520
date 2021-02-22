dad31c49bf3ab1722d6f0b4104755b142420758d
yongyesun <68177406+yongyesun@users.noreply.github.com>
2021-02-22 17:39:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,4 +22,4 @@ sed -i '29a tools-y += ucl upx' tools/Makefile
 sed -i '45a $(curdir)/upx/compile := $(curdir)/ucl/compile' tools/Makefile
 rm -rf package/lede
 sed -i '287,290d' target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-cm520-79f.dts
-sed -i '286a $(printf '\t')\t\t\tpartition@4c80000 {\n\t\t\t\tlabel = "rootfs";\n\t\t\t\treg = <0x4c80000 0x3300000>;\n\t\t\t};\n\ \n\t\t\tpartition@b80000 {\n\t\t\t\tlabel = "rootfs_1";\n\t\t\t\treg = <0xb80000 0x4100000>;\n\t\t\t};' target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-cm520-79f.dts
+sed -i '286a "\t"\t\t\tpartition@4c80000 {\n\t\t\t\tlabel = "rootfs";\n\t\t\t\treg = <0x4c80000 0x3300000>;\n\t\t\t};\n\ \n\t\t\tpartition@b80000 {\n\t\t\t\tlabel = "rootfs_1";\n\t\t\t\treg = <0xb80000 0x4100000>;\n\t\t\t};' target/linux/ipq40xx/files/arch/arm/boot/dts/qcom-ipq4019-cm520-79f.dts
