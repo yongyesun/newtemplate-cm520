f7b14611e4f121a08f8a4f6de422cfd96f6af25b
yongyesun <68177406+yongyesun@users.noreply.github.com>
2021-12-28 20:15:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #####
 sed -i '1,4d' feeds.conf.default
-sed -i '$a src-git packages https://git.openwrt.org/feed/packages.git' feeds.conf.default
-sed -i '$a src-git luci https://git.openwrt.org/project/luci.git' feeds.conf.default
-sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git' feeds.conf.default
-sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git' feeds.conf.default
+sed -i '$a src-git packages https://git.openwrt.org/feed/packages.git^f013af726ad76f928d8188712ae21c8f352ce1f1' feeds.conf.default
+sed -i '$a src-git luci https://git.openwrt.org/project/luci.git^6c167ea880bd29c69730d802c48ebc2c0253b905' feeds.conf.default
+sed -i '$a src-git routing https://git.openwrt.org/feed/routing.git^0f1d89c2e95a428835e269b0c0e832e8675c73e8' feeds.conf.default
+sed -i '$a src-git telephony https://git.openwrt.org/feed/telephony.git^a5d039394014139aac34af1e9460ab76e5779a65' feeds.conf.default
